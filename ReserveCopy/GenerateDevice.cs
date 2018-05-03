da2edc52812df67efc9ec67225aa2651fea24b8f
ЖолдасхановУ <@223_14>
2018-05-03 21:31:53 +0600

    Add project files.

---
+++ ReserveCopy/GenerateDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackupCopy.Dal.Module;
+
+namespace ReserveCopy
+{
+    public class GenerateDevice
+    {
+        public GenerateDevice()
+        {
+            HD = new List<HDD>();
+            DV = new List<DVD>();
+            FL = new List<Flash>();
+        }
+        public List<HDD> HD;
+        public List<DVD> DV;
+        public List<Flash> FL;
+        private Random rand = new Random();
+        public void GenerateFlash()
+        {
+            for (int i = 0; i < rand.Next(1,20); i++)
+            {
+                Flash F = new Flash(string.Format("{Model #{0}", rand.Next()));
+                F.MemoryFlash = rand.Next(2048, 128000);
+                F.typeDevice = (TypeDevice)rand.Next(0, 1);
+                FL.Add(F);
+            }
+        }
+    }
+}
---
+++ ReserveCopy/Program.cs
@@ -0,0 +1,38 @@
+﻿using BackupCopy.Dal.Module;
+using ReserveCopy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // HDD hdd = new HDD();
+           // / hdd.PrintInfo();
+           // Flash flash = new Flash();
+           // // flash.PrintInfo();
+           // hdd.ClusterMemory = 100;
+           // hdd.SpeedHDD = 10;
+           // hdd.Copy();
+            GenerateDevice GenDev = new GenerateDevice();
+            Console.WriteLine("Введите объём данных");
+            double temp = Double.Parse(Console.ReadLine());
+            GenDev.GenerateFlash();
+            Service service = new Service();
+            double totalMemory = service.CalculateTotalMemory(new List<HDD>(), GenDev.FL, new List<DVD>());
+            Console.WriteLine("Общий объём флэшек = {0} ({1})", totalMemory, GenDev.FL.Count);
+            TimeSpan time1 = service.CalculateTotalTime(GenDev.FL);
+            Console.WriteLine(time1.TotalMinutes);
+            service.getCounter(GenDev.FL, temp);
+            foreach (Flash item in GenDev.FL)
+            {
+                item.PrintInfo();
+            }
+        }
+    }
+}
